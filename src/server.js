b50ec948486c773e063037d47e77fbbe86482551
Rafsun83 <rafsun7774@gmail.com>
2025-03-19 00:29:31 +0600

    Added new

--- src/server.js
+++ src/server.js
@@ -18,7 +18,7 @@ app.get('/', (req, res) => {
 });
 
 app.get('/api', (req, res) => {
-  res.json({ message: 'Hello World changes'});
+  res.json({ message: 'Hello World changes new'});
 });
 
 let server;
